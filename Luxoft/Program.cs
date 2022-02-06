990de122e2f07ee1137d97e4fd75f8e6cb2b89f4
AndMusatov <91152285+AndMusatov@users.noreply.github.com>
2022-02-06 17:12:29 +0200

    Complete

---
+++ Luxoft/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Luxoft
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input min:");
+            int min = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Input max:");
+            int max = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Input sum:");
+            int targetlSum = Convert.ToInt32(Console.ReadLine());
+
+            List<int> result = CheckSequence(min, max, targetlSum);
+
+            Console.WriteLine("Result: {0}", string.Join(" ", result));
+        }
+
+        private static List<int> CheckSequence(int min, int max, int targetlSum)
+        {
+            List<int> result = new List<int>();
+
+            ParametrsCheck(ref min, ref max, ref targetlSum);
+
+            for (int i = min; i < max + 1; i++)
+            {
+                var numbers = i.ToString().ToCharArray();
+                var intNumbers = Array.ConvertAll(numbers, c => (int)Char.GetNumericValue(c));
+                int intermediateSum = intNumbers.Sum();
+                if (intermediateSum == targetlSum)
+                {
+                    result.Add(i);
+                }
+            }
+
+            return result;
+        } 
+
+        private static void ParametrsCheck(ref int min, ref int max, ref int targetlSum)
+        {
+            if (min < 0)
+            {
+                min *= -1;
+            }
+
+            if (max < 0)
+            {
+                max *= -1;
+            }
+
+            if (targetlSum < 0)
+            {
+                targetlSum *= -1;
+            }
+        }
+    }
+}
